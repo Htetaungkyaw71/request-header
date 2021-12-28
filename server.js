a60737108e68c0d28cc128aa36e7941373f73dac
hteta <htetaung200071@gmail.com>
2021-12-28 20:49:42 +0630

    initial commit

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var ip = require("ip");
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -21,8 +23,15 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  var i = ip.address();
+  var lang = req.headers["accept-language"];
+  var user_agent = req.get('User-Agent');
+  res.json({
+    ipaddress: i,
+    language:lang,
+    software:user_agent
+    });
 });
 
 
